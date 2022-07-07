458a97cf09d10a5a89c024d0113074a970824262
Kip-Rono <fieldenrono27@gmail.com>
2022-07-07 14:28:23 +0300

    added express framework and ejs templating, controller

---
+++ controllers/todoController.js
@@ -0,0 +1,4 @@
+module.exports = function (app){
+    //set up all request handlers
+
+}
---
+++ server.js
@@ -0,0 +1,8 @@
+//server js
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function (){
+    console.log('Listening on 3000')
+});
